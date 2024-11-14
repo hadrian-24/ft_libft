63702eea8572546a476782f8e1a9ada42206d807
Jadranko Barisic <jabarisi@c4b11c5.42berlin.de>
2024-11-14 15:24:46 +0100

    ft_strtrimm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jabarisi <jabarisi@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:52:31 by jabarisi          #+#    #+#             */
-/*   Updated: 2024/11/14 15:01:30 by jabarisi         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:24:13 by jabarisi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	return (ft_substr(s1, start, end - start));
 }
